d09a680c7dd5479fcd43a527a73220fb8aeebecf
Scott Crowder <scrowder@gmail.com>
2024-01-23 12:03:55 -0600

    Implement SwiftData

--- ConferenceFriends/Friend.swift
+++ ConferenceFriends/Friend.swift
@@ -5,15 +5,27 @@
 //  Created by SCOTT CROWDER on 1/23/24.
 //
 
-import SwiftUI
+import Foundation
+import SwiftData
 
-struct Friend {
+@Model
+final class Friend {
     var name: String
     var notes: String
-    var image: Image?
+    var dateAdded: Date
     
-    #if DEBUG
-    //static let example: Location = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
-    static let example: Friend = Friend(name: "Sample", notes: "This is an example person.", image: Image(systemName: "face.smiling"))
-    #endif
+    var dateFormatted: String {
+        dateAdded.formatted(date: .long, time: .standard)
+    }
+    
+    init(name: String, notes: String) {
+        self.name = name
+        self.notes = notes
+        self.dateAdded = Date()
+    }
+    
+//    #if DEBUG
+//    //static let example: Location = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+//    static let example: Friend = Friend(name: "Sample", notes: "This is an example person.")
+//    #endif
 }
