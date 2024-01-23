ef917ab17db37552b6c0da2c21c5fba87ee3f8c1
Scott Crowder <scrowder@gmail.com>
2024-01-23 12:03:40 -0600

    Implement SwiftData. Update form for better UX

--- ConferenceFriends/AddFriendView.swift
+++ ConferenceFriends/AddFriendView.swift
@@ -10,19 +10,47 @@ import SwiftUI
 struct AddFriendView: View {
     
     @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var modelContext
     
     @State private var name: String = ""
     @State private var notes: String = ""
     
     var body: some View {
         Form {
-            TextField("Name", text: $name)
-            TextField("Notes", text: $notes)
+            VStack(alignment: .leading) {
+                TextField("Name", text: $name)
+                    .textInputAutocapitalization(.words)
+                    .textContentType(.name)
+                Text("*Required")
+                    .font(.caption)
+                    .foregroundStyle(.red)
+            }
+            
+            VStack(alignment: .leading) {
+                TextField("Notes", text: $notes, axis: .vertical)
+                    .textInputAutocapitalization(.sentences)
+                Text("*Required")
+                    .font(.caption)
+                    .foregroundStyle(.red)
+            }
+            
             Button("Save") {
-                dismiss()
+                if save() {
+                    dismiss()
+                }
             }
         }
         .navigationTitle("Add Friend")
+    }
+    
+    func save() -> Bool {
+        
+        guard name != "" else { return false }
+        guard notes != "" else { return false }
+        
+        let newFriend = Friend(name: name, notes: notes)
+        modelContext.insert(newFriend)
+        return true
     }
 }
 
