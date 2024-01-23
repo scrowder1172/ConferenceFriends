b850365186ed3e068397a0b0a1e8cf11849b2604
Scott Crowder <scrowder@gmail.com>
2024-01-23 09:11:30 -0600

    Initial Commit

---
+++ ConferenceFriends/ConferenceFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConferenceFriendsApp.swift
+//  ConferenceFriends
+//
+//  Created by SCOTT CROWDER on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ConferenceFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
