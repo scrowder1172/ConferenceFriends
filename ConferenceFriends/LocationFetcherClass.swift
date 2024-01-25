2d1601b0611bf292d48d566491a76e1bb1b0f905
Scott Crowder <scrowder@gmail.com>
2024-01-25 08:20:03 -0600

    capture user location

---
+++ ConferenceFriends/LocationFetcherClass.swift
@@ -0,0 +1,28 @@
+//
+//  LocationFetcherClass.swift
+//  ConferenceFriends
+//
+//  Created by SCOTT CROWDER on 1/25/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
